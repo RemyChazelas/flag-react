19d941da6a073ce280029b593818a470578064f7
Remy Chazelas <remychazelas@gmail.com>
2021-07-01 11:39:44 +0200

    start flag en react

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>À propos</h1>
+            <br />
+            <p>
+                lorem lorem lorem lorem lorem lorem lorem lorem lorem
+            </p>
+            <br />
+            <p>
+                lorem lorem lorem lorem lorem lorem lorem lorem lorem loremloremloremloremloremloremloremloremloremloremlorem
+            </p>
+        </div>
+    );
+};
+
+export default About;
