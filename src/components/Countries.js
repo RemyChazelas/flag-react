db0e2e0d2985f03a15dc4b80e3494ffe49f05729
Remy Chazelas <remychazelas@gmail.com>
2021-07-06 17:06:13 +0200

    countries sorted

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -5,15 +5,32 @@ import Card from './Card';
 
 const Countries = () => {
     const [data, setData] = useState([]);
+    const [sortedData, setSortedData] = useState([]);
+    const [playOnce, setPlayOnce] = useState(true);
 
     useEffect(() => {
-        axios.get('https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag').then((res) => setData(res.data));
-    }, [])
+        if (playOnce) {
+            axios.get('https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag').then((res) => {
+                setData(res.data)
+                setPlayOnce(false)
+            });
+        }
+
+        const sortedCountry = () => {
+            const countryObj = Object.keys(data).map((i) => data[i]);
+            const sortedArray = countryObj.sort((a, b) => {
+                return b.population - a.population;
+            });
+            sortedArray.length = 30
+            setSortedData(sortedArray)
+        };
+        sortedCountry();
+    }, [data, playOnce])
 
     return (
         <div className="countries">
             <ul className="countries-list">
-                {data.map((country) => (
+                {sortedData.map((country) => (
                     <Card country={country} key={country.name} />
                 ))}
             </ul>
